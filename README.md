3dc461439d1cc171a7d2b67679b567f50de136f4
LucaSalder <luca.kusch@t-online.de>
2024-10-14 19:21:11 +0200

    Kommentare in die Readme Datei

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Game-66
+
+
+- rauben mit 9
+- decken 
+- ohne nachziehen decken
+- hochzeit
+- phase 2 (bedienpflicht und keine hochzeiten und trumpf falls man nicht bedienen kann)
+- spielbewertung
+- letzter stich 10 punkte
+- 65 - 65 unentschieden
+
+
+
+GUI Anlegen
+
+
+
+Strukturgramm
--- ai.py
+++ ai.py
@@ -1,15 +1,3 @@
-"""
-- rauben mit 9
-- decken 
-- ohne nachziehen decken
-- hochzeit
-- phase 2 (bedienpflicht und keine hochzeiten und trumpf falls man nicht bedienen kann)
-- spielbewertung
-- letzter stich 10 punkte
-- 65 - 65 unentschieden
-"""
-
-
 import random
 import numpy as np
 import spiel
